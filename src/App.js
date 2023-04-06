da35702c68c7942faeb74309b6f33cfb0b2cb03b
Kate Kachalouskaya <katerina.kulgun@gmail.com>
2023-04-06 13:16:01 +0300

    I changed design

--- src/App.js
+++ src/App.js
@@ -1,84 +1,58 @@
 import React, { useState, useEffect, useCallback } from "react";
 import "./styles.css";
 import Modal from "./Modal";
-import { useSelector, useDispatch, useStore } from "react-redux";
+import { useDispatch } from "react-redux";
 import Spinner from "./Spinner.js";
 
+let objData1 = {
+  text: "all-meat",
+  countP: "1",
+  countS: "0",
+  startWith: false,
+  format: "json",
+};
 function AppTest() {
   const [showModal, setShowModal] = useState(false);
-  const [text, setText] = useState(() => {
-    const saved = localStorage.getItem("text");
-    const initialValue = JSON.parse(saved);
-    return initialValue || "all-meat";
+  const [objData, setObjData] = useState(() => {
+    const initialValue = JSON.parse(localStorage["text1"]);
+    return initialValue || objData1;
   });
-  const [countP, setCountP] = useState(() => {
-    const saved = localStorage.getItem("countP");
-    const initialValue = JSON.parse(saved);
-    return initialValue || "1";
-  });
-  const [countS, setCountS] = useState(() => {
-    const saved = localStorage.getItem("countS");
-    const initialValue = JSON.parse(saved);
-    return initialValue || "0";
-  });
-  const [format, setFormat] = useState(() => {
-    const saved = localStorage.getItem("format");
-    const initialValue = JSON.parse(saved);
-    return initialValue || "json";
-  });
-  const [startWith, setStartWith] = useState(() => {
-    const saved = localStorage.getItem("startWith");
-    const initialValue = JSON.parse(saved);
-    return initialValue || false;
-  });
-  useEffect(() => {
-    localStorage.setItem("startWith", JSON.stringify(startWith));
-  }, [startWith]);
-  useEffect(() => {
-    localStorage.setItem("text", JSON.stringify(text));
-  }, [text]);
-  useEffect(() => {
-    localStorage.setItem("countP", JSON.stringify(countP));
-  }, [countP]);
-  useEffect(() => {
-    localStorage.setItem("countS", JSON.stringify(countS));
-  }, [countS]);
-  useEffect(() => {
-    localStorage.setItem("format", JSON.stringify(format));
-  }, [format]);
-  const [data, setData] = useState("");
 
+  useEffect(() => {
+    localStorage["text1"] = JSON.stringify(objData);
+  }, [objData]);
+
+  const [data, setData] = useState("");
   const dispatch = useDispatch();
-  const handler = useCallback(() => {
-    dispatch({ type: "DATA_TYPE", payload: data });
-  }, [data]);
+  const [loading, setLoading] = useState(false);
+  const [valid, setValid] = useState("");
 
   const handler1 = useCallback(() => {
     setShowModal(true);
   }, [data]);
+
   function validation(p) {
     if (Number(p) != p) {
       setShowModal(false);
       return <p>Введите число</p>;
     }
   }
-  const [loading, setLoading] = useState(false);
-  const [valid, setValid] = useState("");
+
   const funcGetDate = async () => {
     setLoading(true);
     await fetch(
       "https://baconipsum.com/api/?type=" +
-        text +
+        objData.text +
         "&paras=" +
-        countP +
+        objData.countP +
         "&sentences=" +
-        countS +
-        (startWith ? "&start-with-lorem=1" : "") +
+        objData.countS +
+        (objData.startWith ? "&start-with-lorem=1" : "") +
         "&format=" +
-        format
+        objData.format
     )
       .then((response) => {
-        if (format == "json") {
+        if (objData.format == "json") {
           return response.json();
         } else {
           return response.text();
@@ -90,61 +64,63 @@ function AppTest() {
       });
     setLoading(false);
     handler1();
-    setValid(validation(countP));
-    setValid(validation(countS));
-    dispatch({ type: "FORMAT_TYPE", payload: format })
+    setValid(validation(objData.countP));
+    setValid(validation(objData.countS));
+    dispatch({ type: "FORMAT_TYPE", payload: objData.format });
   };
+
+  function changeNote(index, event) {
+    objData[index] = event.target.value;
+    setObjData({ ...objData });
+  }
 
   return (
     <div>
       <div id="main">
-        <div class="span">type </div>
+        <div className="span">type </div>
         <select
-          class="inp"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
+          className="inp"
+          value={objData.text}
+          onChange={(event) => changeNote("text", event)}
         >
           <option>all-meat</option>
           <option>meat-and-filler</option>
         </select>
-        <div class="span">paras </div>
+        <div className="span">paras </div>
         <input
-          class="inp"
-          value={countP}
+          className="inp"
+          value={objData.countP}
           placeholder="paras"
-          onChange={(event) => {
-            setCountP(event.target.value);
-          }}
+          onChange={(event) => changeNote("countP", event)}
         />
         {valid}
-        <div class="span">sentences </div>
+        <div className="span">sentences </div>
         <input
-          class="inp"
-          value={countS}
+          className="inp"
+          value={objData.countS}
           placeholder="sentences"
-          onChange={(event) => {
-            setCountS(event.target.value);
-          }}
+          onChange={(event) => changeNote("countS", event)}
         />
         {valid}
-        <div class="span1">
+        <div className="span1">
           <input
-            class="inp1"
+            className="inp1"
             id="check1"
             type="checkbox"
-            checked={startWith}
+            checked={objData.startWith}
             onChange={(event) => {
-              setStartWith(!startWith);
+              objData["startWith"] = !objData.startWith;
+              setObjData({ ...objData });
             }}
           />
-          <label for="check1">start-with-lorem</label>
+          <label htmlFor="check1">start-with-lorem</label>
         </div>
 
-        <div class="span">format </div>
+        <div className="span">format </div>
         <select
-          class="inp"
-          value={format}
-          onChange={(event) => setFormat(event.target.value)}
+          className="inp"
+          value={objData.format}
+          onChange={(event) => changeNote("format", event)}
         >
           <option>json</option>
           <option>text</option>
